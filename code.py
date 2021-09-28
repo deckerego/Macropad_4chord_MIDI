987971e36b9e3550c4a367570a688d2a6ffe616a
John Ellis <john@deckerego.net>
2021-09-27 20:55:16 -0400

    Show currently pressed notes

--- code.py
+++ code.py
@@ -8,10 +8,12 @@ macropad.pixels.fill(0x0F0F0F)
 
 progression = settings.conf['progression']
 text_lines = macropad.display_text("Macropad 4chord MIDI")
-text_lines[1].text = str(progression)
+text_lines[1].text = "Chords: " + ' '.join(progression)
+text_lines[2].text = "Pressed: "
 text_lines.show()
 
 def key_pressed(event):
+    global pressed_notes
     row = key_event.key_number // 3
     column = key_event.key_number % 3
     note = chords[row][column]
@@ -19,12 +21,15 @@ def key_pressed(event):
     if key_event.pressed:
         macropad.midi.send(macropad.NoteOn(note, settings.conf['velocity']))
         macropad.pixels[key_event.key_number] = 0x00FF00
-        text_lines[2].text = "NoteOn: %s" % Key.to_name(note)
+        pressed_notes.append(note)
 
     if key_event.released:
         macropad.midi.send(macropad.NoteOff(note, 0))
         macropad.pixels[key_event.key_number] = 0x0F0F0F
-        text_lines[2].text = "NoteOff: %s" % key.to_name(note)
+        pressed_notes.remove(note)
+
+    note_names = [Key.to_name(note) for note in pressed_notes]
+    text_lines[2].text = "Pressed: " + ' '.join(note_names)
 
 def switch_key(position_change):
     global key, chords
@@ -38,6 +43,8 @@ def switch_key(position_change):
 key = None
 chords = None
 encoder_last_position = 0
+pressed_notes = []
+
 switch_key(encoder_last_position)
 
 while True:
