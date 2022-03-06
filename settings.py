d96b9bcdd406d751fb78e83f260b689f805d0c27
John Ellis <532789+deckerego@users.noreply.github.com>
2022-03-05 21:33:07 -0500

    Updated chord progressions

--- settings.py
+++ settings.py
@@ -7,11 +7,12 @@ conf = {
     'keys': [ "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"],
     # The list of progressions you can select by press & turning on the rotary dial
     'progressions': [
-        ["I", "V", "vi", "IV"], # The "pop" progression
-        ["vi", "IV", "I", "V"], # A rotation of the "pop" progression
-        ["IV", "V", "I", "vi"], # Coldplay progression
-        ["I", "vi", "IV", "V"], # The '50s progression
-        ["I", "IV", "ii", "V"], # The Montgomery Ward bridge
-        ["I", "iii", "vi", "V"] # Blues progression
+        ["I",  "V",   "vi", "IV"], # The "pop" progression
+        ["I",  "IV",  "vi", "V" ], # Alternate Western pop
+        ["vi", "IV",  "I",  "V" ], # Minor pop progression
+        ["IV", "V",   "I",  "vi"], # Coldplay progression
+        ["I",  "vi",  "IV", "V" ], # The '50s progression
+        ["I",  "IV",  "ii", "V" ], # The Montgomery Ward bridge
+        ["I",  "iii", "vi", "V" ]  # Blues progression
     ]
 }
