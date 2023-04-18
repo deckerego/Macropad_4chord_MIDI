c80dab69166c63c8b3739622a1dfea61e7cfb602
John Ellis <532789+deckerego@users.noreply.github.com>
2023-04-18 17:08:22 -0400

    Create a tick event for timed execution

--- code.py
+++ code.py
@@ -21,13 +21,14 @@ mode_current = 0
 encoder_last_position = 0
 encoder_last_down = False
 encoder_last_pressed = 0
-last_time_seconds = time.time()
+last_time_seconds = time.monotonic()
 sleep_seconds = settings.display['sleep_seconds']
+sleep_active = False
 
 # Seconds (floating) since last invocation
 def elapsed_seconds():
     global last_time_seconds
-    current_seconds = time.time()
+    current_seconds = time.monotonic()
     elapsed_seconds = current_seconds - last_time_seconds
     last_time_seconds = current_seconds
     return elapsed_seconds
@@ -45,6 +46,16 @@ def click_event():
     mode_current = (mode_current + 1) % 4
     refresh()
 
+def wake_event():
+    global sleep_seconds, sleep_active
+    sleep_seconds = settings.display['sleep_seconds']
+    sleep_active = False
+
+def sleep_event():
+    global sleep_active
+    modes[mode_current].sleep_event()
+    sleep_active = True
+
 refresh()
 while True:
     # Process all available key events at once
@@ -55,21 +66,27 @@ while True:
         key_event = macropad.keys.events.get()
     if key_events:
         modes[mode_current].keypad_events(key_events)
-        sleep_seconds = settings.display['sleep_seconds']
+        wake_event()
 
     if macropad.encoder_switch != encoder_last_down:
         if not macropad.encoder_switch and encoder_last_pressed > 0:
             click_event()
         encoder_last_down = macropad.encoder_switch
         encoder_last_pressed = time.time() if macropad.encoder_switch else 0
-        sleep_seconds = settings.display['sleep_seconds']
+        wake_event()
     elif macropad.encoder != encoder_last_position:
         modes[mode_current].rotate_event(macropad.encoder, encoder_last_position, macropad.encoder_switch);
         encoder_last_position = macropad.encoder
         encoder_last_pressed = 0
-        sleep_seconds = settings.display['sleep_seconds']
+        wake_event()
+    
+    elapsed = elapsed_seconds()
+    if hasattr(modes[mode_current], 'tick') and not sleep_active: 
+        modes[mode_current].tick(elapsed)
 
-    # Let the keypad go dark if it has timed out
-    if sleep_seconds: # Always on if None
-        sleep_seconds -= elapsed_seconds()
-        if sleep_seconds <= 0: modes[mode_current].sleep_event()
+    if sleep_seconds:
+        sleep_seconds -= elapsed
+        if not sleep_active and sleep_seconds <= 0: 
+            sleep_event()
+
+    if sleep_active: time.sleep(1.0)
